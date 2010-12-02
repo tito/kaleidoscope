690d82d17570bde1e16c89343c1bc9b93b32beed
Michel Blanc <mblanc@erasme.org>
2010-12-02 18:52:13 +0100

    set SO_REUSEADDR on server socket

--- kaleidoscope/server.py
+++ kaleidoscope/server.py
@@ -292,6 +292,7 @@ class KalServer(asyncore.dispatcher):
     def __init__(self, host, port):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
         self.bind((host, port))
         self.listen(4)
 
